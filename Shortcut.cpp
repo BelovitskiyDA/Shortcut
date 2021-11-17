248dcaeff528057b0e4441e45d01eb0c53194c0e
BelovitskiyDA <89989289+BelovitskiyDA@users.noreply.github.com>
2021-11-17 21:13:14 +0300

    funct startAllAlgorithm

--- Shortcut.cpp
+++ Shortcut.cpp
@@ -10,6 +10,7 @@ void printMainMenu()
 		<< "[3] Save weights matrix" << endl
 		<< "[4] Print weights matrix" << endl
 		<< "[5] Finding shortcut" << endl
+		<< "[6] Run the database" << endl
 		<< "[0] Exit" << endl
 		<< "Choose action: ";
 }
@@ -225,6 +226,13 @@ int findShortcut(const vector<vector <double>>& matrix)
 	}
 }
 
+void startAllAlgorithm(vector<vector <double>> matrix, int startPoint, int endPoint)
+{
+	startAlgorithm(algorithmDijkstra, matrix, startPoint, endPoint);
+	startAlgorithm(algorithmFord, matrix, startPoint, endPoint);
+	startAlgorithm(algorithmFloyd, matrix, startPoint, endPoint);
+}
+
 int main()
 {
 	vector<vector <double>> matrix;
@@ -232,7 +240,7 @@ int main()
 	while (true)
 	{
 		printMainMenu();
-		switch (inputNumber(0, 5))
+		switch (inputNumber(0, 6))
 		{
 		case 1:
 		{
@@ -307,6 +315,33 @@ int main()
 			{
 				cout << "Error: matrix not create" << endl;
 			}
+			break;
+		}
+		case 6:
+		{
+			int i = 0;
+			ifstream fin;
+			bool flag = true;
+
+			while (flag)
+			{
+				string fileName = "matrix base\\" + to_string(++i);
+				fin.open(fileName, ios::in);
+				if (fin.is_open())
+				{
+					flag = fin.is_open();
+					cout << "file: " << fileName << endl;
+					matrix = loadMatrix(fin);
+					startAllAlgorithm(matrix, 0, 1);
+				}
+				else
+				{
+					cout << "Error of weights matrix load from file:" << i << endl;
+					flag = false;
+				}
+				fin.close();
+			}
+
 			break;
 		}
 		case 0:
