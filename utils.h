828333a9fa5e2779a4f4c5c22d1a5b465829b35c
ChernikovaEkaterina <92220708+ChernikovaEkaterina@users.noreply.github.com>
2021-11-19 17:32:44 +0300

    error

--- utils.h
+++ utils.h
@@ -19,5 +19,6 @@ void printMatrix(const vector<vector <double>>& matrix);
 vector<vector <double>> createMatrix();
 int selectStartPoint(int size);
 int selectEndPoint(int size);
+void algorithmFloyd(vector<vector <double>> matrix, int startPoint, int endPoint);
 int findShortcut(const vector<vector <double>>& matrix);
 
