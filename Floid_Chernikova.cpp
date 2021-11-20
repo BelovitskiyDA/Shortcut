66d616635f15a8d189d56cf0d7e679b831bc7d38
TheP1ch <beluakov.eugene@bk.ru>
2021-11-20 18:30:17 +0300

    Dijktras's algorithm done

--- Floid_Chernikova.cpp
+++ Floid_Chernikova.cpp
@@ -1,18 +1,18 @@
 #include "utils.h"
 void algorithmFloyd(vector<vector <double>> matrix, int startPoint, int endPoint)
 {
-	int n = size(matrix);
+	int n = matrix.size();
 	for (int i = 0; i < n; i++)      
 		for (int j = 0; j < n; j++)
 		{
 			if (i != j && matrix[i][j] == 0)
-				matrix[i][j] = INFINITY;
+				matrix[i][j] = std::numeric_limits<double>::infinity();
 		}
 	cout << "algorithmFloyd" << endl;
 	for (int k = 0; k < n; k++)
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < n; j++)
-				if (matrix[i][k] < INFINITY && matrix[k][j] < INFINITY) {
+				if (matrix[i][k] < std::numeric_limits<double>::infinity() && matrix[k][j] < std::numeric_limits<double>::infinity()) {
 					matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
 					if (matrix[i][i] < 0) {
 						cout << "Floyd's algorithm does not work correctly in the presence of a negative weight cycle" << endl;
